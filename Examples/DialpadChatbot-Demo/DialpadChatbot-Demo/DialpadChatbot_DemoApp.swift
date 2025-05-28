e7fa36bdd477508071da9e4b185a9f97c6a1bb69
Dhruba Baishya <dhruba.baishya@dialpad.com>
2025-05-27 21:30:58 -0700

    feat(dialbot): restructure ios client (#7)
    ### Context: https://dialpad.atlassian.net/browse/DP-142096
    
    ### Changelog
    - [x] refactor: restructure ios client
    - [ ] todo: update doc

--- Examples/DialpadChatbot-Demo/DialpadChatbot-Demo/DialpadChatbot_DemoApp.swift
+++ Examples/DialpadChatbot-Demo/DialpadChatbot-Demo/DialpadChatbot_DemoApp.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @main
-struct ExampleApp: App {
+struct DialpadChatbot_DemoApp: App {
     var body: some Scene {
         WindowGroup {
             /**
@@ -10,7 +10,10 @@ struct ExampleApp: App {
              * with your production URL, which can be obtained from the Digital Experience admin pages.
              * Alternatively, you can reach out to the professional services team to get your production URL.
              */
-            ContentView(url: "https://lighthouse.dx.dialpad.com/dxclient/dist/?provemail=3340&channelid=1b6db43e5ecf4354a9e41ccd2621b05c&native=ios")
+            ContentView(
+                url:
+                    "https://lighthouse.dx.dialpad.com/dxclient/dist/?provemail=3340&channelid=3f3fd91bc5bf4e9ab56e53c516933337&native=ios"
+            )
         }
     }
 }
